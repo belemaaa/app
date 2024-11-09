5f098af8a9ff2ca76bc5e6a7da5cead9f2d54522
belemaaa <graceitamunoala@gmail.com>
2024-11-09 14:34:22 +0100

    initial

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,25 @@
+<script lang="ts">
+	// import '../app.css'
+</script>
+
+<div class="body">
+	<nav>
+		<ul>
+			<li>
+				<a href="/">Home</a>
+			</li>
+			<li>
+				<a href="/posts">Posts</a>
+			</li>
+		</ul>
+	</nav>
+
+	<slot />
+</div>
+
+<style>
+	.body {
+		margin-left: 10px;
+		margin-right: 10px;
+	}
+</style>
---
+++ src/routes/posts/+layout.svelte
@@ -0,0 +1,29 @@
+<div class="posts">
+	<aside>
+		<h4>Posts</h4>
+
+		<nav>
+			<ul>
+				<li>
+					<a href="/posts/one">post 1</a>
+				</li>
+				<li>
+					<a href="/posts/two">post 2</a>
+				</li>
+			</ul>
+		</nav>
+	</aside>
+
+	<main>
+		<slot />
+	</main>
+</div>
+
+<style>
+	.posts {
+		display: grid;
+		gap: 2rem;
+		grid-template-columns: 200px 60ch;
+		margin-top: 2rem;
+	}
+</style>
